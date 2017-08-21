089b38c89f9e38febffd508f75a61831c6e80f06
Sylvain MATHIEU <sylvain.mathieu@orange.com>
2017-08-21 10:54:11 +0000

    update frame flow

--- server/capture.js
+++ server/capture.js
@@ -7,6 +7,12 @@ var lb = Buffer.allocUnsafe(4);
 var timer = null;
 var running = false;
 
+
+var latency = {
+    capture: [],
+    encode: []
+}
+
 var options = {
     inputWidth: 0,
     inputHeight: 0,
@@ -47,6 +53,20 @@ module.exports.start = function(distantWidth, distantHeight, codecWidth, codecHe
     options.bit_rate = bandwidth;
     options.fps = fps;
     timer = setInterval(getFrame, 1000 / options.fps);
+
+    latencyTimer = setInterval(displayCurrentLatency, 1000);
+}
+
+
+function displayCurrentLatency() {
+    var capLat = latency.capture / latency.capture.length;
+    var encLat = latency.encode / latency.encode.length;
+    console.log("desktop capture latency : " + capLat + " encoder latency : " + encLat + " global latency : " + (capLat + encLat));
+
+    var latency = {
+        capture: [],
+        encode: []
+    }
 }
 
 module.exports.stop = function() {
@@ -81,20 +101,32 @@ function getFrame() {
         running = true;
     }
 
-    var frame = encoder.encodeFrameSync(img.data);
-    if (frame !== undefined) {
-        if (socket.getSocket() == null){
- free(); }
-	else {
+    if (frameSend) {
+        frameSend = false;
+        var frame = encoder.encodeFrameSync(img.data);
 
-	//TODO android patch 
-	lb.writeInt32LE(frame.length);
-	socket.getSocket().write(lb);	
-	console.log(frame.length);
-        socket.getSocket().write(frame);
-        var frameTime = new Date();
-        //console.log("getImage Time:", getImageTime - initTime, "encoder time : ", frameTime - getImageTime, "global send time : ", frameTime - initTime);
-}
+        if (frame !== undefined) {
+
+            var frameTime = new Date();
+            latency.capture.push(getImageTime - initTime);
+            latency.encode.push(frameTime - getImageTime);
+
+            if (socket.getSocket() == null) {
+                free();
+            } else {
+
+                //TODO android patch 
+                lb.writeInt32LE(frame.length);
+                socket.getSocket().write(lb, function() {
+                    console.log(frame.length);
+                    socket.getSocket().write(frame, function() {
+                        frameSend = true;
+                    });
+                });
+
+
+            }
+        }
     }
 }
 
