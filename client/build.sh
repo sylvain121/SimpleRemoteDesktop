cfe99a403a2b7422162449cf1f766f802704c266
wreuven <wreuven@users.noreply.github.com>
2020-09-03 14:15:37 +0300

    Update build.sh

--- client/build.sh
+++ client/build.sh
@@ -1,2 +1,3 @@
+sudo apt-get install libsdl2-dev libsdl2-net-dev libavformat-dev
 gcc -o client src/client.c src/input.c src/video_decoder.c src/video_surface.c src/network.c src/keysym_converter.c -Wall -lavutil -lavformat -lavcodec -lz -lavutil -lm -lswscale -g -lSDL2 -lSDL2_net
 
